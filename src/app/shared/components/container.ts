03f2d6fcbeecd79f7ada282c326af860fdc82c3f
GeronimoGM <geronimogonzalezmartino@gmail.com>
2026-06-29 19:00:13 -0300

    feat: add HlmBadge component and update container component

--- src/app/shared/components/container.ts
+++ src/app/shared/components/container.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { classes } from '@spartan-ng/helm/utils';
 
 @Component({
   selector: '[app-container]',
   imports: [],
   template: `<ng-content />`,
   host: {
-    class: 'container mx-auto max-w-7xl p-md sm:px-lg lg:px-xl',
+    class: 'container mx-auto max-w-7xl',
   },
 })
-export class Container {}
+export class Container {
+  readonly class = input('');
+
+  constructor() {
+    classes(() => [this.class()]);
+  }
+}
